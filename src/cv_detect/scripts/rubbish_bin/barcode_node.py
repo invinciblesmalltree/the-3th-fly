76b1a646e9c893def24c4badc37a05a0b9c74f68
Hiromichi <2271612727@qq.com>
2024-05-26 20:22:16 +0800

    v4

--- src/cv_detect/scripts/rubbish_bin/barcode_node.py
+++ src/cv_detect/scripts/rubbish_bin/barcode_node.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
 import rospy
-import time
 import cv2
 import numpy as np
 from pyzbar.pyzbar import decode
-import pyrealsense2 as rs
+import time
 from cv_bridge import CvBridge
 from cv_detect.msg import BarMsg
 from sensor_msgs.msg import Image
@@ -17,32 +16,20 @@ def decode_barcode(image):
     barcode_data = barcodes[0].data.decode('utf-8')
     return int(barcode_data)
 
-def d435_2cv2():
-    pipeline = rs.pipeline()
-    config = rs.config()
-    config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
-    config.enable_stream(rs.stream.depth, 1280, 720, rs.format.z16, 30)
-    pipeline.start(config)
-    while True:
-        frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
-        depth_frame = frames.get_depth_frame()
-
-        if color_frame is not None and depth_frame is not None:
-            color_image = np.asanyarray(color_frame.get_data())
-            depth_image = np.asanyarray(depth_frame.get_data())
-
-            return color_image, depth_image
-        
-        rospy.sleep(0.1)
+global frame
+frame = None
+def callback(data, bridge=CvBridge()):
+    global frame
+    if data is not None:
+        frame = bridge.imgmsg_to_cv2(data, "bgr8")
 
 def main():
     rospy.init_node('barcode_node', anonymous=True)
+    rgb_sub = rospy.Subscriber('/d435/rgb', Image, callback)
     pub = rospy.Publisher('barcode_msg', BarMsg, queue_size=10)
     rate = rospy.Rate(20)
 
-    while True:
-        frame, depth_image = d435_2cv2()
+    while(1):
         if frame is not None:
             height, width = frame.shape[:2]
 
